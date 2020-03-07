c141bc020d9887be54ec96416d60bff17806f0ed
Jayson Scruggs <jayson.scruggs@gmail.com>
2020-03-07 13:50:43 -0800

    Added Metar pull

---
+++ data_controller/__init__.py
@@ -0,0 +1,52 @@
+from requests import get
+from bs4 import BeautifulSoup as Soup
+from enum import Enum
+
+class Colors(Enum):
+    VFR = (255, 0, 0)
+    MVFR = (0, 0, 255)
+    IFR = (0, 255, 0)
+    LIFR = (0, 125, 125)
+    COLOR_CLEAR = (0, 0, 0)
+
+
+
+stations = {
+    'KBOS': 1,
+    'KATL': 0
+}
+
+# COLOR_VFR = (255, 0, 0)				# Green
+# COLOR_MVFR = (0, 0, 255)				# Blue
+# COLOR_IFR = (0, 255, 0)				# Red
+# COLOR_LIFR = (0, 125, 125)			# Magenta
+
+
+def _request_metar_data():
+    url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&"
+    url = url + '&'.join(
+        [
+            'requestType=retrieve',
+            'format=xml&hoursBeforeNow=5',
+            'mostRecentForEachStation=true',
+            'fields=flight_category,station_id',
+            'stationString=' + ','.join(stations.keys())
+        ]
+    )
+    r = get(url)
+    return r.content.decode('utf-8', errors='replace')
+
+
+def _parse_metar_data(data: str) -> dict:
+    soup = Soup(data)
+    data = dict()
+    for ele in soup.find_all('metar'):
+        data[ele.find('station_id').get_text()] = getattr(Colors, ele.find('flight_category').get_text(), Colors.COLOR_CLEAR).value
+    return data
+
+
+
+if __name__ == '__main__':
+    print(Colors.VFR.value)
+    with open('test.data', 'r') as f:
+        print(_parse_metar_data(f.read()))
