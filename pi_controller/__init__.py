0727f859ac185830f7b8d3a3b64334fb99e373bf
Jayson Scruggs <jayson.scruggs@gmail.com>
2020-09-19 15:13:22 -0500

    added test data

--- pi_controller/__init__.py
+++ pi_controller/__init__.py
@@ -6,9 +6,11 @@ import neopixel
 from ..data_controller import METARData
 
 
+TOTAL_LIGHTS = 250
+
 class RunLights:
     def __init__(self):
-        self.pixels = neopixel.NeoPixel(board.D18, 250, brightness=.5, pixel_order=neopixel.RGB)
+        self.pixels = neopixel.NeoPixel(board.D18, TOTAL_LIGHTS, brightness=.5, pixel_order=neopixel.RGB)
 
     def __enter__(self):
         return self
@@ -22,5 +24,7 @@ class RunLights:
         with open(metar.station_data, 'r') as f:
             for i, station in enumerate(f.readlines()):
                 self.pixels[i] = data[station.strip()]
+                if i + 1 == TOTAL_LIGHTS:
+                    break
         self.pixels.show()
         time.sleep(interval)
